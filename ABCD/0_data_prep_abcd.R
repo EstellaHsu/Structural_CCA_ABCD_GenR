e9bdb2196853f0ed60da4993b53e2949d5d640a3
Bing Xu <73080574+EstellaHsu@users.noreply.github.com>
2023-03-09 15:25:35 +0100

    Update and rename abcd_qc_demo_cbcl.R to 0_data_prep_abcd.R

--- ABCD/0_data_prep_abcd.R
+++ ABCD/0_data_prep_abcd.R
@@ -2,68 +2,63 @@
 ############## ABCD QC ##################
 #########################################
 
-
-setwd("/Users/estella/Desktop/ABCD_download")
+# first read all the QC and covariates data
+# a small function to rename all the ids to match the ids of the brain data in our server
+rename_idc <- function(x){ sapply(as.character(x$src_subject_id), 
+                           function(x) paste0("sub-NDAR", strsplit(x, "_")[[1]][2]))}
 
 #########################################
 #demographic info
 #########################################
 participants <- read.delim(file = 'abcd_lpds01.txt')
 names(participants)
-head(participants)
-
-# rename idc for merging data
-participants$idc <- sapply(as.character(participants$src_subject_id), 
-                           function(x) paste0("sub-NDAR", strsplit(x, "_")[[1]][2]))
+participants$idc <- rename_idc(participants)
 
 # only 1 year follow up had the parental education data
 prnt_ed <- participants[-1,c("idc","demo_prnt_ed_v2_l","eventname")]
 prnt_ed1 <- prnt_ed %>% filter(as.character(eventname) == "1_year_follow_up_y_arm_1")
-#table(prnt_ed$demo_prnt_ed_v2_l[prnt_ed$eventname == "1_year_follow_up_y_arm_1"])
-table(prnt_ed$eventname)
 
-# other data we need baseline
+# other data we need baseline (age 10)
 participants1 <- participants %>% filter(as.character(eventname) == "baseline_year_1_arm_1")
+# extract ids, sex, and age data
 participants2 <- participants1[, c("idc","sex","interview_age")]
 
 # merge the useful variables
 demo_info <- merge(prnt_ed1[,c("idc","demo_prnt_ed_v2_l")],participants2,by="idc")
 
 #########################################
-#Site info
+#Site information
 #########################################
-
 site <- read.delim(file = 'abcd_lt01.txt')
-site$idc <- sapply(as.character(site$src_subject_id), 
-                   function(x) paste0("sub-NDAR", strsplit(x, "_")[[1]][2]))
+names(site)                                
+site$idc <- rename_idc(site)
 site1 <- site %>% filter(as.character(eventname) == "baseline_year_1_arm_1")
+# extract the study site information
 site_info <- site1[, c("idc","site_id_l")]
 
 
 #########################################
-#genetic: twins & siblings
+#twins & siblings information
 #########################################
-
 twin <- read.delim("acspsw03.txt")
 names(twin)
 
-twin$idc <- sapply(as.character(twin$src_subject_id), function(x) paste0("sub-NDAR", strsplit(x, "_")[[1]][2]))
+twin$idc <- rename_idc(twin)                  
 twin1 <- twin %>% filter(eventname == "baseline_year_1_arm_1")
+# extract the race & ethnicity, and family ids (used for exlude twins/siblings/triples)           
 twin_info <- twin1[,c("idc","race_ethnicity","rel_family_id")]
 
 
 #########################################
 #brain data QC
 #########################################
-
-setwd("/Users/estella/Desktop/ABCD_download/manual_qc")
+                   
 qc_str <- read.delim("abcd_imgincl01.txt")
 names(qc_str)
 
-qc_str$idc <- sapply(as.character(qc_str$src_subject_id), 
-                               function(x) paste0("sub-NDAR", strsplit(x, "_")[[1]][2]))
+qc_str$idc <- rename_idc(qc_str)
 qc1 <- qc_str %>% filter(eventname == "baseline_year_1_arm_1")
-
+# extract the recommended QC variable
 strqc_info1 <- qc1[,c("idc","imgincl_t1w_include")]
 
 
@@ -74,10 +69,10 @@ setwd("/Users/estella/Desktop/ABCD_download")
 qc_findings <- read.delim("abcd_mrfindings02.txt")
 names(qc_findings)
 
-qc_findings$idc <- sapply(as.character(qc_findings$src_subject_id), 
-                     function(x) paste0("sub-NDAR", strsplit(x, "_")[[1]][2]))
+qc_findings$idc <- rename_idc(qc_findings)
 qc2 <- qc_findings %>% filter(eventname == "baseline_year_1_arm_1")
 
+#extract the incidental finding scroes
 strqc_info2 <- qc2[,c("idc","mrif_score")]
 str(strqc_info2)
 
@@ -92,32 +87,32 @@ df_list <- list(strqc_info1, strqc_info2, twin_info, site_info, demo_info)
 all_info <- df_list %>% reduce(full_join, by='idc')
 dim(all_info)
 
-# 1. id has str data
-setwd("/Users/estella/Desktop/Structural_cca")
+# 1. ids who have the brain structral data on our server
 id <- readRDS("id_has_strdata.rds")
 all_info <- all_info %>% filter(idc %in% id)
 dim(all_info)
 
-# 1. inclusion criteria
+# 1. inclusion criteria: based on the inclusion critaria of ABCD and remove the children with clinical relevant incidental findings
 all_info1 <- all_info %>% filter(imgincl_t1w_include == 1 & mrif_score < 3)
 dim(all_info1)
 
-# 2. remove twin/siblings
+# 2. remove twin/siblings/triples
 all_info2 <- all_info1[!duplicated(all_info1$rel_family_id), ]
 dim(all_info2)
 
 
+                                  
 #########################################
-#merge CBCL
+#merge CBCL: behavioral assessment
 #########################################
-
-setwd("/Users/estella/Desktop/ABCD_download/ABCD_CBCL")
 cbcl <- read.delim("abcd_cbcls01.txt")
-cbcl$idc <- sapply(as.character(cbcl$src_subject_id),function(x) paste0("sub-NDAR", strsplit(x, "_")[[1]][2]))
+cbcl$idc <- rename_idc(cbcl)
 
 cbcl1 <- cbcl %>% filter(as.character(eventname) == "baseline_year_1_arm_1")
 names(cbcl1)
-######## extract syndrome scale 
+                                  
+                                  
+######## extract syndrome scale scores, internalizing, externalizing and total problems raw scoress
 cbcl_syn <- cbcl1[, c("idc","cbcl_scr_syn_anxdep_r","cbcl_scr_syn_withdep_r","cbcl_scr_syn_somatic_r",
                       "cbcl_scr_syn_social_r","cbcl_scr_syn_thought_r","cbcl_scr_syn_attention_r",
                       "cbcl_scr_syn_rulebreak_r","cbcl_scr_syn_aggressive_r","cbcl_scr_syn_internal_r",
@@ -125,11 +120,12 @@ cbcl_syn <- cbcl1[, c("idc","cbcl_scr_syn_anxdep_r","cbcl_scr_syn_withdep_r","cb
 dim(cbcl_syn)
 
 
-###### merge all the data
+###### merge all the data: cbcl & post-qc data
 
 all_cbcl <- merge(all_info2, cbcl_syn,by="idc")
 str(all_cbcl)
 
+# the structrue of the original data is all characters, so we transformed some of them to numeric data
 all_cbcl1 <- all_cbcl %>% mutate_at(c("interview_age","cbcl_scr_syn_anxdep_r","cbcl_scr_syn_withdep_r","cbcl_scr_syn_somatic_r",
                          "cbcl_scr_syn_social_r","cbcl_scr_syn_thought_r","cbcl_scr_syn_attention_r",
                          "cbcl_scr_syn_rulebreak_r","cbcl_scr_syn_aggressive_r","cbcl_scr_syn_internal_r",
@@ -139,13 +135,13 @@ colSums(is.na(all_cbcl1))
 
 # there are many NAs in the final data in parental education, sex, interview age
 # so try to read the ABCC participants data
-setwd("/Users/estella/Desktop/ABCD_download")
 dd_rsmri <- read.table(file = 'participants.tsv', sep = '\t', header = TRUE)
 names(dd_rsmri)[1] <- "idc"
 # use the parental edu, sex, and age from this dataset. 
 rsmri <- dd_rsmri[, c("idc","sex","age","parental_education","site")]
-
 apply(rsmri,2,table)
+                                  
+                                  
 # fill the missingness
 na_demo <- all_cbcl1[is.na(all_cbcl1$demo_prnt_ed_v2_l), ]
 dim(na_demo)
@@ -161,10 +157,6 @@ all_final0 <- all_cbcl1 %>%
 dim(all_final0)
 all_final0$sex[all_final0$sex == 1] <- "M"
 all_final0$sex[all_final0$sex == 2] <- "F"
-
-#cc <- all_final0[,c("idc","interview_age","sex","demo_prnt_ed_v2_l")]
-#cbind(cc[40:70,], all_cbcl1[40:70,c("idc","interview_age","sex","demo_prnt_ed_v2_l")])
-#na_fill[na_fill$idc == "sub-NDARINV05WK8AN7", ]
 
 names(all_final0)[6:7] <- c("site","parental_education")
 
@@ -199,146 +191,28 @@ dim(all_final_noNA)
 saveRDS(all_final_noNA,"all_final_noNA_update.rds")
 
 
-########################################
-#Add ICV, total brain volumes
-#########################################
-setwd("/Users/estella/Desktop/Structural_cca/")
-
-icv <- read.delim("abcd_smrip10201.txt")
-names(icv)
-icv$idc <- sapply(as.character(icv$src_subject_id), 
-                          function(x) paste0("sub-NDAR", strsplit(x, "_")[[1]][2]))
-icv2 <- icv %>% filter(eventname == "baseline_year_1_arm_1")
-
-icv_tbv <- icv2[,c("idc","smri_vol_scs_intracranialv","smri_vol_scs_suprateialv")]
-
-str(icv_tbv)
-
-
-all_final_noNA <- merge(all_final_noNA, icv_tbv, by="idc")
-dim(all_final_noNA)
-str(all_final_noNA)
-
-all_final_noNA <- all_final_noNA %>% mutate_at(c("smri_vol_scs_intracranialv","smri_vol_scs_suprateialv"), as.numeric)
-str(all_final_noNA)
-colSums(is.na(all_final_noNA))
-
-saveRDS(all_final_noNA,"all_final_abcd_tbv.rds")
 
 ########################################
-#train test split: site
+#train test split based on study sites
 #########################################
 
 set.seed(519)
 # first split the site 10 times
 sites <- unique(all_final_noNA$site)
-# there are 22 sites, so 22*0.8=17.6 for the training sites
+# there are 21 sites, so 21*0.8=16.8 for the training sites
 train_sites <- lapply(1:10, function(i) sample(sites, size = 17, replace = FALSE))
 
 # then split of the ids
 all_final_train <- lapply(1:10, function(i) all_final_noNA %>% filter(site %in% train_sites[[i]]))
 all_final_test <- lapply(1:10, function(i) all_final_noNA %>% filter(!site %in% train_sites[[i]]))
 
-lapply(all_final_train, nrow)
-names(all_final_test[[1]])
-class(all_final_train[[1]])
+
+saveRDS(all_final_train,"all_final_train.rds")
+saveRDS(all_final_test,"all_final_test.rds")
 
 
-saveRDS(all_final_train,"all_final_train_tbv.rds")
-saveRDS(all_final_test,"all_final_test_tbv.rds")
-
-setwd("/Users/estella/Desktop/Structural_cca/")
-
-all_final_train <- readRDS("all_final_train_tbv.rds")
-all_final_test <- readRDS("all_final_test_tbv.rds")
-
-train <- all_final_train[[1]]
-test <- all_final_test[[1]]
-
-summary(test)
-apply(test,2,sd)
-##############################################################################################################################3
-#####################################################################################
-################# merge CBCL items
-#####################################################################################
-
-setwd("/Users/estella/Desktop/ABCD_download/ABCD_CBCL")
-cbcl <- read.delim("abcd_cbcl01.txt")
-cbcl$idc <- sapply(as.character(cbcl$src_subject_id),function(x) paste0("sub-NDAR", strsplit(x, "_")[[1]][2]))
-
-cbcl1 <- cbcl %>% filter(as.character(eventname) == "baseline_year_1_arm_1")
-names(cbcl1)
-######## extract syndrome scale 
-cbcl_item <- cbcl1[, c(132,10:128)]
-dim(cbcl_item)
-
-
-###### merge all the data
-
-all_cbcl_item <- merge(all_info2, cbcl_item,by="idc")
-str(all_cbcl_item)
-names(all_cbcl_item)
-
-all_cbcl_item[,10:128] <- apply(all_cbcl_item[,10:128],2,as.numeric)
-
-colSums(is.na(all_cbcl_item))
-
-# there are many NAs in the final data in parental education, sex, interview age
-# so try to read the ABCC participants data
-setwd("/Users/estella/Desktop/ABCD_download")
-dd_rsmri <- read.table(file = 'participants.tsv', sep = '\t', header = TRUE)
-names(dd_rsmri)[1] <- "idc"
-# use the parental edu, sex, and age from this dataset. 
-rsmri <- dd_rsmri[, c("idc","sex","age","parental_education","site")]
-names(all_cbcl_item)[6] <- "site"
-
-all_final <- merge(rsmri, all_cbcl_item[,!names(all_cbcl_item) %in% c("sex","interview_age","demo_prnt_ed_v2_l")], by=c("idc","site"))
-
-all_final[all_final == 777] <- NA
-all_final[all_final == 888] <- NA
-all_final[all_final == 999] <- NA
-all_final[all_final == ""] <- NA
-
-colSums(is.na(all_final))
-sum(duplicated(all_final$idc))
-
-# remove data with NAS in relevant variables
-idx_noNA <- which(rowSums(is.na(all_final[, !names(all_final) %in% c("imgincl_t1w_include","mrif_score","rel_family_id")])) == 0)
-
-all_final_noNA <- all_final[idx_noNA, ]
-
-# change the data type
-str(all_final_noNA)
-
-all_final_noNA <- all_final_noNA %>% mutate_at(c("sex","site","race_ethnicity","parental_education"), as.factor)
-str(all_final_noNA)
-colSums(is.na(all_final_noNA))
-
-# adjust the level of parental education
-levels(all_final_noNA$parental_education) <- list("low" = 1:12,"medium" = 13:17,"high" = 18:21)
-str(all_final_noNA)
-dim(all_final_noNA)
-
-saveRDS(all_final_noNA,"all_final_noNA_item.rds")
-
-names(all_final_noNA)
-########################################
-#train test split: site
-#########################################
-# first split the site 10 times
-sites <- unique(all_final_noNA$site)
-# there are 22 sites, so 22*0.8=17.6 for the training sites
-train_sites <- lapply(1:10, function(i) sample(sites, size = 17, replace = FALSE))
-
-# then split of the ids
-all_final_train_item <- lapply(1:10, function(i) all_final_noNA %>% filter(site %in% train_sites[[i]]))
-all_final_test_item <- lapply(1:10, function(i) all_final_noNA %>% filter(!site %in% train_sites[[i]]))
-
-lapply(all_final_train_item, nrow)
-class(all_final_train[[1]])
-saveRDS(all_final_train_item,"all_final_train_item.rds")
-saveRDS(all_final_test_item,"all_final_test_item.rds")
-
+                         
+                        
 
 
 
