e2fea05f1270ac249e308bd0682f4f43104aaf99
Bing Xu <73080574+EstellaHsu@users.noreply.github.com>
2023-11-14 13:41:33 +0100

    Update 1.Residulization.R

--- GenR/1.Residulization.R
+++ GenR/1.Residulization.R
@@ -2,8 +2,8 @@
 ########### data residulization ###########
 ###########################################
 
-setwd("V:/medewerkers/051950 Xu, B/PhD projects/Structural_CCA")
-all_final <- readRDS("all_final_genr_zscores_30.01.2023.rds")
+setwd("V:/medewerkers/****** Xu, B/PhD projects/Structural_CCA")
+all_final <- readRDS("all_final_genr_zscores.rds")
 names(all_final)
 # brain measures are 13:87
 # regress the feature matrix on age, gender, ethnicity and SES (maternal education)
@@ -17,11 +17,11 @@ brain_residual <- do.call(cbind, resi_brain)
 #brain_residual <- cbind(IDC=all_final$IDC, brain_residual)
 colnames(brain_residual) <- names(all_final)[12:120]
 head(brain_residual)
-saveRDS(brain_residual, "brain_residual_tbv_zscores_09.01.2023.rds")
+saveRDS(brain_residual, "brain_residual_zscores.rds")
 
 cbcl <- all_final[,c(1:8)]
 df_all <- cbind(cbcl,brain_residual)
 
 names(df_all)
 head(df_all)
-saveRDS(df_all, "df_all_genr_30.01.2023.rds")
+saveRDS(df_all, "df_all_genr.rds")
